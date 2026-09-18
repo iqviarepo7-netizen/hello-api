33b3e19b3b7fd628da51ff86763130d609ffcda2
mohan.kumar2 <mohan.kumar2@iqvia.com>
2026-09-19 00:18:48 +0530

    aidlc: update 2 file(s)

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/hello_world_i_am_mk")
-def hello_world():
+@app.get("/hello_world_i_am_mk", response_model=dict)
+async def hello_world_i_am_mk():
     return {"message": "Hello, world! I am mk"}
